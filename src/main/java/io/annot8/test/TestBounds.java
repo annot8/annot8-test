97bfdba7dad51c9e737441f4eb45ebb26dfac337
Chris Flatley <chris@committed.io>
2018-02-08 14:03:04 +0000

    Update testbounds to core 0.1-SNAPSHOT

--- src/main/java/io/annot8/test/TestBounds.java
+++ src/main/java/io/annot8/test/TestBounds.java
@@ -1,7 +1,22 @@
 package io.annot8.test;
 
 import io.annot8.core.bounds.Bounds;
+import io.annot8.core.data.Content;
+import java.util.Optional;
 
 public class TestBounds implements Bounds {
 
+  // TODO: Add mocked responses for this perhaps (eg map of requriedClass to value for getData)
+
+  @Override
+  public <D, C extends Content<D>, R> Optional<R> getData(C content, Class<R> requiredClass) {
+    // Can't return anything
+    return Optional.empty();
+  }
+
+  @Override
+  public <D, C extends Content<D>> boolean isValid(C content) {
+    // true for everything
+    return true;
+  }
 }
