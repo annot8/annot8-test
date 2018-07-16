d50e9b415ae5a0d26b228c435f1d2546e4249eb1
Steven Taylor <steven@committed.io>
2018-07-16 18:20:41 +0100

    Opens the testing module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
-@SuppressWarnings("module")
-    module io.annot8.test {
+@SuppressWarnings("module") open module io.annot8.test {
   requires io.annot8.core;
   requires io.annot8.common;
+
+  exports io.annot8.test;
   exports io.annot8.test.content;
 }
