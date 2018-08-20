c3a5ed16a48d1649d3cdc2fa09be1dd86c04ed7f
Chris Flatley <chris@committed.io>
2018-08-20 16:04:09 +0200

    Introduce annot8-testing parent

--- annot8-test-impl/src/main/java/module-info.java
+++ annot8-test-impl/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 open module io.annot8.test {
-  requires transitive io.annot8.common;
+  requires transitive io.annot8.common.components;
 
   exports io.annot8.test;
   exports io.annot8.test.content;
