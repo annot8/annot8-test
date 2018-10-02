d352c7f957e83b84304250a9ecca68fa3edc76e6
Chris Flatley <chris@committed.io>
2018-10-02 22:07:59 +0100

    TestItemFactory now saves items by default

--- annot8-test-impl/src/main/java/io/annot8/testing/testimpl/TestItemFactory.java
+++ annot8-test-impl/src/main/java/io/annot8/testing/testimpl/TestItemFactory.java
@@ -1,18 +1,31 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.testing.testimpl;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import io.annot8.core.data.Item;
 import io.annot8.core.data.ItemFactory;
 
 public class TestItemFactory implements ItemFactory {
 
+  private List<Item> createdItems = new LinkedList<>();
+
   @Override
   public Item create() {
-    return new TestItem();
+    TestItem i = new TestItem();
+    createdItems.add(i);
+    return i;
   }
 
   @Override
   public Item create(Item parent) {
-    return new TestItem(parent.getId());
+    TestItem i = new TestItem(parent.getId());
+    createdItems.add(i);
+    return i;
+  }
+
+  public List<Item> getCreatedItems() {
+    return createdItems;
   }
 }
